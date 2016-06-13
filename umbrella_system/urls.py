b30838845d9b684fd39e4eb778f63d8f6d4be76e
平間 峻徳 <130138@jc-21.jp>
2016-06-13 09:30:20 +0900

    no message

---
+++ umbrella_system/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'umbrella_system'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    # ex: /umbrella_system/201606/date/
+    url(r'^(?P<dates>[0-9]+)/date/$', views.date, name='date'),
+]
