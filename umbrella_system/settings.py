00efd1f68147b182f0a00b849f907ef911477719
伊藤 修也 <130575@jc-21.jp>
2016-07-07 15:11:26 +0900

    no message

--- umbrella_system/settings.py
+++ umbrella_system/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_nose',
-    'team2.umbrella_system',
+    'team2_umbrella_system.umbrella_system',
 ]
 
 MIDDLEWARE_CLASSES = [
